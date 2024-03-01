92b2400bce5ba84fac8bbde148024addbc8e44b1
eksmeks <144058360+eksmeks@users.noreply.github.com>
2024-03-01 18:12:42 +0100

    Update streamlit_user_input_on.py

--- streamlit_user_input_on.py
+++ streamlit_user_input_on.py
@@ -2,13 +2,17 @@
 
 import streamlit as st
 
-def main():
-    user_input = st.text_input(" ", value=" ")
-    with open('website_text_content.txt', 'a', encoding='utf-8') as file:
-        file.write(str(user_input+"\n"))
-    with open('website_text_content.txt', 'r', encoding='utf-8') as file1:
-        stryng = file1.read()
-    st.text(stryng)
+input_file_path = 'website_text_content.txt'
 
+def main():
+    with open('website_text_content.txt', 'r', encoding='utf-8') as file:
+        stryng = file.read()
+    st.text(stryng)
+    user_input = st.text_input(" ", value="-")
+    if st.button("txt", key="scroll_button"):
+        if user_input not in stryng:
+            with open('website_text_content.txt', 'a', encoding='utf-8') as file:
+                file.write(str(user_input+"\n"))
+    
 if __name__ == "__main__":
     main()
